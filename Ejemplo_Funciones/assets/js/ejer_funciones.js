f07cd668801d0e8c16c0078a67342311033c06f9
velasco182 <ruben.velasco0@misena.edu.co>
2023-05-05 18:36:34 -0500

    Ejercicio Funciones

--- Ejemplo_Funciones/assets/js/ejer_funciones.js
+++ Ejemplo_Funciones/assets/js/ejer_funciones.js
@@ -15,7 +15,7 @@ function inicio(){
     nombre = prompt("Ingresa tu usuario: ");
     pw = prompt("Ingresa tu contraseña: ");
 
-    if(nombre === registro.nom && pw === registro.pwd){
+    if(nombre == registro().nom && pw == registro().pwd){
         alert("Inicio de sesión exitoso.")
         console.log(`Bienvenido ${nombre}, te encuentras en ${ciudad}.`)
 
