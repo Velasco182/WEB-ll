9b34abf9192c3ebac993dda9c193db172d3f9ddd
velasco182 <ruben.velasco0@misena.edu.co>
2023-05-26 14:46:37 -0500

    Estructuras de Control

---
+++ Estructuras_Decisivas/assets/js/main.js
@@ -0,0 +1,10 @@
+// const edad = 18;
+const edad = prompt("Ingresa tu edad");
+
+if(edad>=18){
+    console.log("Eres mayor de edad");
+    document.write("Eres mayor de edad");
+}else{
+    console.log("Eres menor de edad");
+    document.write("Eres menor de edad");
+}
